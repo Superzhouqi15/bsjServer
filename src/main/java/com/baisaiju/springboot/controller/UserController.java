a2be05c9cd9ee2a93b0e245d09d92f81992c0795
dav1dch <807557852@qq.com>
2020-01-31 01:43:53 +0800

    get openId

--- src/main/java/com/baisaiju/springboot/controller/UserController.java
+++ src/main/java/com/baisaiju/springboot/controller/UserController.java
@@ -3,12 +3,58 @@ package com.baisaiju.springboot.controller;
 import com.baisaiju.springboot.dao.UserTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class UserController {
 
     @Autowired
     private UserTemplate userTemplate;
+
+    @ResponseBody
+    @PostMapping("/getOpenId")
+    public String getOpenId(@RequestBody Map<String, Object> data) throws IOException {
+        String code = data.get("code").toString();
+        System.out.println(code);
+        File dir = new File("");
+        List<String> result = new ArrayList<>();
+        try {
+            System.out.println(dir.getAbsolutePath().replace("\\", "\\\\"));
+            Process process = Runtime.getRuntime()
+                    .exec("python " + dir.getAbsolutePath() + "/python/getOpenid.py" + " " + code);
+            process.waitFor();
+            BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream(), "UTF-8"));
+            BufferedReader in1 = new BufferedReader(new InputStreamReader(process.getErrorStream(), "UTF-8"));
+            String line = null;
+            while ((line = in.readLine()) != null) {
+                result.add(line);
+                System.out.println(line);
+            }
+            while ((line = in1.readLine()) != null) {
+                System.out.println(line);
+            }
+            in.close();
+            // java代码中的process.waitFor()返回值为0表示我们调用python脚本成功，
+            // 返回值为1表示调用python脚本失败，这和我们通常意义上见到的0与1定义正好相反
+            int re = process.waitFor();
+            System.out.println(re);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println(result.toString());
+        return result.get(0);
+    }
+
 
 
 }
